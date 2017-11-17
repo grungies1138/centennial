74d639cb456e64ca921247e8c833440cd8edd497
grungies1138 <grungies1138@gmail.com>
2017-11-17 16:46:51 -0600

    testing Web redesign

--- web/characterview/urls.py
+++ web/characterview/urls.py
@@ -1,7 +1,7 @@
 # URL patterns for the character app
 
 from django.conf.urls import url
-from web.character.views import sheet
+from web.characterview.views import sheet
 
 urlpatterns = [
     url(r'^sheet/(?P<object_id>\d+)/$', sheet, name="sheet")
