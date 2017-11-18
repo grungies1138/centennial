db3c31715617e2294f6dcff67ce5052b94bf1d18
grungies1138 <grungies1138@gmail.com>
2017-11-17 20:08:59 -0600

    testing Web redesign

--- web/character/views.py
+++ web/character/views.py
@@ -1,6 +1,18 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
+from django.http import Http404
 from django.shortcuts import render
+from django.conf import settings
 
-# Create your views here.
+from evennia.utils.search import object_search
+from evennia.utils.utils import inherits_from
+
+
+def sheet(request, object_id):
+    object_id = '#' + object_id
+    try:
+        character = object_search(object_id)[0]
+    except IndexError:
+        raise Http404("I couldn't find a character with that ID.")
+    if not inherits_from(character, settings.BASE_CHARACTER_TYPECLASS):
+        raise Http404("I couldn't find a character with that ID. "
+                      "Found something else instead.")
+    return render(request, 'character/sheet.html', {'character': character})
