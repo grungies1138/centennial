081b2e2f5929c1a3b3f9317df3d0b3e80cbc3d76
grungies1138 <grungies1138@gmail.com>
2017-11-20 16:37:41 -0600

    modifying the web page

---
+++ world/weapon_prototypes.py
@@ -0,0 +1,13 @@
+DL_17 = {
+    "key": "DL-17",
+    "typeclass": "typeclasses.weapon.Weapon",
+    "health": 20,
+    "max_health": 20,
+    "skill": "Firearms",
+    "accuracy": 30,
+    "damage": 15,
+    "damage_type": "Energy",
+    "durability": 10,
+    "mass": 2,
+    "manufacturer": "Blastech Industries"
+}
