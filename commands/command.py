47ce191f6ef5c594c4a07cabf8c0aa1b3917911b
grungies1138 <grungies1138@gmail.com>
2017-11-21 16:56:42 -0600

    modifying the web page

--- commands/command.py
+++ commands/command.py
@@ -80,7 +80,7 @@ class SheetCommand(BaseCommand):
         else:
             percent = 0
 
-        target.msg("Current Percentage: %s" % percent)
+        target.msg("Current Percentage: %s" % (current / max_health))
         target.msg("Max Health: %s" % max_health)
 
         if percent > 75:
