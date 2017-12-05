58866fa793c17a6edbfe621e850ed99dafa7eadc
grungies1138 <grungies1138@gmail.com>
2017-12-05 14:14:39 -0600

    modifying the web page

--- world/languages.py
+++ world/languages.py
@@ -8,7 +8,7 @@ def setup_languages():
 def add_binary():
     phonemes = "w b d t oe, ee, oo"
     vowels = "oei"
-    grammar = "cvvv cvv cvvcv cvvcvv cvvvc cvvvcvv cvvc"
+    grammar = "cvvv cvv cvvcv cvvcvv cvvvc cvvvcvv cvvc c v cc vv"
     word_length_variance = 4
     rplanguage.add_language(key='Binary', phonemes=phonemes, grammar=grammar,
                             word_length_variance=word_length_variance, vowels=vowels, force=True)
