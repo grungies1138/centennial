faffb9b652216f4cb04ee8c58332b2448ab9a7b5
grungies1138 <grungies1138@gmail.com>
2017-12-05 14:56:24 -0600

    modifying the web page

--- commands/command.py
+++ commands/command.py
@@ -847,7 +847,7 @@ class CmdOOC(default_cmds.MuxCommand):
 
         if self.caller.db.status == 'IC':
             self.caller.db.ic_location = self.caller.location
-            self.caller.move_to("Limbo")
+            self.caller.move_to(#0)
             self.caller.db.status == 'OOC'
         else:
             self.caller.msg("You are already OOC.  type |w+ic|n to return to the IC world.")
