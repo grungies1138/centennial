798e098faf1dda2a499faf75ac9cb1db80245990
grungies1138 <grungies1138@gmail.com>
2017-12-05 15:00:00 -0600

    modifying the web page

--- commands/command.py
+++ commands/command.py
@@ -847,7 +847,7 @@ class CmdOOC(default_cmds.MuxCommand):
 
         if self.caller.db.status == 'IC':
             self.caller.db.ic_location = self.caller.location
-            limbo = self.caller.search("Limbo")
+            limbo = self.caller.search("#0")
             self.caller.move_to(limbo)
             self.caller.db.status = 'OOC'
         else:
