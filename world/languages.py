1c3abb0e7438733f0b4dc4de1fa1ad3d78212e0f
grungies1138 <grungies1138@gmail.com>
2018-01-18 10:23:33 -0600

    added language setup to server start.
    Added Shyriiwook to language list

--- world/languages.py
+++ world/languages.py
@@ -30,7 +30,7 @@ def add_shyriiwook():
     phonemes = "wh wrr wr grr gr ah aa oo ugh ee arr a e o"
     vowels = "aeo"
     grammar = "ccv ccvv ccvvv ccvvvv cccv cccvv cccvvv cccvvvv ccvcc ccvccc ccvvcc ccvvvcc ccvvvccc cccvvvcc " \
-              "cccvvvccc cv cc"
+              "cccvvvccc cv cc c v"
     word_length_variance = 3
     rplanguage.add_language(key='Shyriiwook', phonemes=phonemes, vowels=vowels, grammar=grammar,
                             word_length_variance=word_length_variance, force=True)
@@ -39,7 +39,7 @@ def add_shyriiwook():
 def add_huttese():
     phonemes = "bh sh d b s wh w h a e i o u wh w"
     vowels = "aeiou"
-    grammar = "cvv ccvv ccvvc ccvvcc ccvvccv cvvc cvvcv cvvcvcc ccvvcvcc cc vv"
+    grammar = "cvv ccvv ccvvc ccvvcc ccvvccv cvvc cvvcv cvvcvcc ccvvcvcc cc vv c v"
     word_length_variance = 2
     rplanguage.add_language(key='Huttese', phonemes=phonemes, vowels=vowels, grammar=grammar,
                             word_length_variance=word_length_variance, force=True)
