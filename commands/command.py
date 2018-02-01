2a14c2461adf6b3a7cd08e264201129c5687bfcf
grungies1138 <grungies1138@gmail.com>
2018-02-01 15:02:36 -0600

    +pose command

--- commands/command.py
+++ commands/command.py
@@ -943,6 +943,7 @@ class CmdPose(default_cmds.MuxCommand):
         spoken = self.caller.db.spoken_lang
 
         chars = self.caller.location.online_characters()
+        print(str(chars))
         speakers = [char if spoken in char.db.languages else '' for char in chars]
         nonspeakers = [char if char not in speakers else '' for char in chars]
 
