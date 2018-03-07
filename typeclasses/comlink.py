2a29a7766b102b1fe2f777699678a39b6e01b5dd
grungies1138 <grungies1138@gmail.com>
2018-03-07 14:33:05 -0600

    Comlinks

--- typeclasses/comlink.py
+++ typeclasses/comlink.py
@@ -118,7 +118,7 @@ class Comlink(Object):
     def msg(self, text="", from_obj=None, **kwargs):
         print("Message received: %s" % self.location.key)
         msgobj = kwargs.get("msgobj")
-        print("Handling Message Object: %s" % str(kwargs))
+        print("Handling Message Object: %s" % msgobj)
         if msgobj:
             freq = msgobj.senders[0].key
             passwd = self.db.passwords.get(freq) or None
