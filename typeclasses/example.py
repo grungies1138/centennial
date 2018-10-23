e79cf2b27d1ad93ec315334efece245524d02897
grungies1138 <grungies1138@gmail.com>
2018-10-23 12:52:28 -0500

    making some small adjustments to weapon damage and combat commands.
    Added vendor file with specs.
    
    Small correction to connection screen.

--- typeclasses/example.py
+++ typeclasses/example.py
@@ -1,9 +1,8 @@
-class BaseObject:
-    def __init__(self):
-        pass
+class Human:
+    self.arms = 2
+    self.legs = 2
+    self.eyes = 2
+    self.hair = ""
+    def __init__(self, hair):
+        self.hair = hair
 
-    self.name = "base"
-
-
-class Derived(BaseObject):
-    pass
