99cad1a8c4c091f6101a0a7a1ff134544077c831
grungies1138 <grungies1138@gmail.com>
2018-10-23 13:08:58 -0500

    making some small adjustments to weapon damage and combat commands.
    Added vendor file with specs.
    
    Small correction to connection screen.

--- typeclasses/example.py
+++ typeclasses/example.py
@@ -1,7 +1,11 @@
-class Human:
+class Mammal:
     self.arms = 2
     self.legs = 2
     self.eyes = 2
+    def __init__(self):
+        pass
+
+class Human:
     self.hair = ""
     def __init__(self, hair):
         self.hair = hair
