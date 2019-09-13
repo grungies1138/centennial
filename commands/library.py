08efa1141aa410fef9aa050363883f36b93d7087
grungies1138 <grungies1138@gmail.com>
2019-09-13 13:50:36 -0500

    0.9 migration

--- commands/library.py
+++ commands/library.py
@@ -31,10 +31,10 @@ def notify(caller, lockstring, message):
 def header(header_text=None, width=MAX_WIDTH, fill_char=HEAD_CHAR):
     header_string = ""
     if header_text and len(header_text) < width:
-        header_repeat = (width - len(header_text)) / 2
+        header_repeat = (width - len(header_text)) // 2
         header_string = fill_char * header_repeat + header_text + fill_char * header_repeat
         if header_string < width:
-            header_string += fill_char * width - len(header_string)
+            header_string += fill_char * (width - len(header_string))
 
     else:
         header_string = fill_char * width
